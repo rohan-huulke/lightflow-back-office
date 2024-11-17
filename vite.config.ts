196b01e6e029a69183c27e9ebfc31153ca27a698
huulke_rohan <r.verma@huulke.com>
2024-11-17 13:17:44 +0530

    feat: Add new theme implementation

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
+import laravel from "laravel-vite-plugin";
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ["resources/js/src/main.ts"],
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    includeAbsolute: false,
+                },
+            },
+        }),
+        VueI18nPlugin({
+            include: path.resolve("resources/js/src/locales/**"),
+        }),
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve("./resources/js/src"),
+        },
+    },
+    optimizeDeps: {
+        include: ["quill"],
+    },
+});
